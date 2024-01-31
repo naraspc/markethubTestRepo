90706b7115b4f321ed634f2cd97b0c107460930a
Jinwook Kim <wodnrtkfka@naver.com>
2024-01-31 22:16:03 +0900

    Revert "Feat: item"

--- src/main/java/org/hanghae/markethub/domain/item/repository/ItemRepository.java
+++
@@ -1,7 +0,0 @@
-package org.hanghae.markethub.domain.item.repository;
-
-import org.hanghae.markethub.domain.item.entity.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRepository extends JpaRepository<Item, Long> {
-}
--- src/main/java/org/hanghae/markethub/domain/store/repository/StoreRepository.java
+++
@@ -1,7 +0,0 @@
-package org.hanghae.markethub.domain.store.repository;
-
-import org.hanghae.markethub.domain.store.entity.Store;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StoreRepository extends JpaRepository<Store, Long> {
-}
