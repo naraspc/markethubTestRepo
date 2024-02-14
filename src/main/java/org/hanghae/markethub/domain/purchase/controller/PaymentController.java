9579e0b3c89d3a9bdfaf76e4da843bf50163a0e2
leegitjw <bachu7410@gmail.com>
2024-02-14 22:15:17 +0900

    fix: html 수정 및 dto 변경

--- src/main/java/org/hanghae/markethub/domain/purchase/controller/PaymentController.java
+++ src/main/java/org/hanghae/markethub/domain/purchase/controller/PaymentController.java
@@ -29,16 +29,16 @@ public class PaymentController {
 
     private final PurchaseService purchaseService;
     private final ItemService itemService;
-    private final RedissonFairLock redissonFairLock;
+    //private final RedissonFairLock redissonFairLock;
     private final IamportClient iamportClient;
 
-    public PaymentController(PurchaseService purchaseService, ItemService itemService,RedissonFairLock redissonFairLock) {
+    public PaymentController(PurchaseService purchaseService, ItemService itemService) {
         this.itemService = itemService;
         String secretKey = "KuT8n5XYtxPTo4c0VoRTQLrZeHJUOsx3h7zBXgrltDcL6yiH7KZ5ulZJVJWPeqRvPxfuE5B7u1G7Ioxc";
         String apiKey = "4067753427514612";
         this.purchaseService = purchaseService;
         this.iamportClient = new IamportClient(apiKey, secretKey);
-        this.redissonFairLock = redissonFairLock;
+       // this.redissonFairLock = redissonFairLock;
     }
 
 
