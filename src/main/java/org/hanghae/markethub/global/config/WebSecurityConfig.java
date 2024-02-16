39d60889539fb245eaa8acd349d9eb9524b2f1da
jay <daonj0715@gmail.com>
2024-02-16 16:55:21 +0900

    Fix

--- src/main/java/org/hanghae/markethub/global/config/WebSecurityConfig.java
+++ src/main/java/org/hanghae/markethub/global/config/WebSecurityConfig.java
@@ -67,10 +67,9 @@ public class WebSecurityConfig {
                 authorizeHttpRequests
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // resources 접근 허용 설정
                         .requestMatchers("/api/user/**","static/**").permitAll() // '/api/user/'로 시작하는 요청 모두 접근 허가
-                        .requestMatchers("/api/cart/**").permitAll()
+                        .requestMatchers("/api/carts/**").permitAll()
                         .requestMatchers("/api/items/**").permitAll()
                         .requestMatchers("/api/item").permitAll()
-                        .requestMatchers("*").permitAll()
 //                        .requestMatchers(HttpMethod.POST).permitAll()
 //                        .requestMatchers(HttpMethod.GET).permitAll()
                         .anyRequest().authenticated() // 그 외 모든 요청 인증처리
