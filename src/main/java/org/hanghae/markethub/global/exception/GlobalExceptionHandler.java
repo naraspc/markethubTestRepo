f7500a899f3ad41fd34ff4fe6f87c0b92ac9bf2b
mingtian-chan <sniwoo98@naver.com>
2024-02-26 16:20:50 +0900

    feat: IllegalArgumentException 처리 추가

--- src/main/java/org/hanghae/markethub/global/exception/GlobalExceptionHandler.java
+++ src/main/java/org/hanghae/markethub/global/exception/GlobalExceptionHandler.java
@@ -14,6 +14,11 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleGeneralException(Exception ex) {
