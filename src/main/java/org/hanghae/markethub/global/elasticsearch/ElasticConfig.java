257167f09c60c6c30cf772706ea21b594a203f5c
jay <daonj0715@gmail.com>
2024-02-29 20:17:08 +0900

    Fix:ElastConfig

--- src/main/java/org/hanghae/markethub/global/elasticsearch/ElasticConfig.java
+++ src/main/java/org/hanghae/markethub/global/elasticsearch/ElasticConfig.java
@@ -1,6 +1,5 @@
 package org.hanghae.markethub.global.elasticsearch;
 
-import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
