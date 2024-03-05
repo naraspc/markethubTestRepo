48a12f5d21f1c9405eb14007d3c2c0e68e797d91
jay <daonj0715@gmail.com>
2024-03-05 17:55:10 +0900

    fix:cicd https

--- src/main/java/org/hanghae/markethub/global/config/WebSecurityConfig.java
+++ src/main/java/org/hanghae/markethub/global/config/WebSecurityConfig.java
@@ -59,27 +59,32 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         // CSRF 설정
-//        http.csrf(AbstractHttpConfigurer::disable);
-        http.csrf((csrf) -> csrf.disable());
+        http.csrf(AbstractHttpConfigurer::disable);
 
         // 기본 설정인 Session 방식은 사용하지 않고 JWT 방식을 사용하기 위한 설정
         http.sessionManagement((sessionManagement) ->
                 sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         );
 
+//        http.authorizeHttpRequests((authorizeHttpRequests) ->
+//                authorizeHttpRequests
+//                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // resources 접근 허용 설정
+//                        .requestMatchers("/api/user/**","static/**").permitAll() // '/api/user/'로 시작하는 요청 모두 접근 허가
+////                        .requestMatchers(HttpMethod.POST,"/api/user/**","static/**").authenticated()
+//                        .requestMatchers("/api/carts/**").permitAll()
+//                        .requestMatchers("/api/items/**").permitAll()
+//                        .requestMatchers("/api/payment/token").permitAll()
+//                        .requestMatchers("/").permitAll()
+//                        .requestMatchers("/api/stores/**").permitAll()
+//                        .requestMatchers("/api/event/**").permitAll()
+//                        .requestMatchers("/img/**").permitAll()
+//                        .anyRequest().authenticated() // 그 외 모든 요청 인증처리
+//        );
+
         http.authorizeHttpRequests((authorizeHttpRequests) ->
-                authorizeHttpRequests
-                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // resources 접근 허용 설정
-                        .requestMatchers("/api/user/**","static/**").permitAll() // '/api/user/'로 시작하는 요청 모두 접근 허가
-//                        .requestMatchers(HttpMethod.POST,"/api/user/**","static/**").authenticated()
-                        .requestMatchers("/api/carts/**").permitAll()
-                        .requestMatchers("/api/items/**").permitAll()
-                        .requestMatchers("/api/payment/token").permitAll()
-                        .requestMatchers("/").permitAll()
-                        .requestMatchers("/api/stores/**").permitAll()
-                        .requestMatchers("/api/event/**").permitAll()
-                        .requestMatchers("/img/**").permitAll()
-                        .anyRequest().authenticated() // 그 외 모든 요청 인증처리
+                        authorizeHttpRequests
+                                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // resources 접근 허용 설정
+                                .requestMatchers("/**").permitAll()
         );
 
         http.addFilterBefore(corsFilter(), ChannelProcessingFilter.class);
